42fbb83560b186cb683bc93dd593412dc40272cf
Benzazoua <billalben252@gmail.com>
2024-04-06 21:43:00 +0100

    Routing, Layout, and Navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
 import ReactDOM from "react-dom/client";
+import AppRouter from "@routes/AppRouter";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<div>hello</div>);
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@styles/global.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<AppRouter />);
